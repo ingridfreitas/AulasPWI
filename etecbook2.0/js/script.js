74c67829e5b73f35eaac2045bffa35b4b6f86845
Ingrid Heloise Santos Freitas <61015082+ingridfreitas@users.noreply.github.com>
2020-12-04 12:39:07 -0300

    EtecbookEdit

---
+++ etecbook2.0/js/script.js
@@ -0,0 +1,19 @@
+var nome = 'Ingrid Heloise Santos Freitas';
+var senha = 20092004;
+$('#enviar').onclick(function(){
+    if ($('#passw').val() = senha) {
+        window.open('index.html');
+    }
+    else {
+        alert("Senha Incorreta");
+    }
+});
+function sign(){
+    alert(document.getElementById('user').value);
+    if(document.getElementById('user') = nome){
+        window.open('index.html');
+    }
+    else{
+        alert("Usuário não encontrado")
+    }
+};
