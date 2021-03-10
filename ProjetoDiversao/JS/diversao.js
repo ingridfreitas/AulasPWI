094f9381f8f099ff9d07d4942befa463ac987e4f
Ingrid Heloise Santos Freitas <ingrid.freitas15@etec.sp.gov.br>
2021-03-10 14:41:03 -0300

    10-03

--- ProjetoDiversao/JS/diversao.js
+++ ProjetoDiversao/JS/diversao.js
@@ -1,6 +1,38 @@
+var nums = 1;
+imagem [1] = "img/series/himym.jpg"
+imagem [2] = "img/series/tbbt.jpg"
+imagem [3] = "img/series/vikings2.jpg"
 function rolserie(){
-    imagem [1] = "img/series/himym.jpg"
-    imagem [2] = "img/series/tbbt.jpg"
-    imagem [3] = "img/series/vikings.jpg"
-    var ver = imagem [1, 2, 3];
+    document.getElementById("serie").src=imagem[nums];
+    nums++;
+    if(nums == 4){
+        nums = 1;
+    }
 };
+setInterval('rolserie()',2000);
+
+var numf = 5;
+imagem [5] = "img/filmes/arthur2.jpg"
+imagem [6] = "img/filmes/branquelas.jpg"
+imagem [7] = "img/filmes/ragnarok2.jpg"
+function rolfilm(){
+    document.getElementById("filme").src=imagem[numf];
+    numf++;
+    if(numf == 8){
+        numf = 5;
+    }
+};
+setInterval('rolfilm()',2000);
+
+var numd = 9;
+imagem [9] = "img/desenhos/gum.jpg"
+imagem [10] = "img/desenhos/winx.jpg"
+imagem [11] = "img/desenhos/xmen.jpg"
+function roldes(){
+    document.getElementById("desenho").src=imagem[numd];
+    numd++;
+    if(numd == 12){
+        numd = 9;
+    }
+};
+setInterval('roldes()',2000);
